e64200d55e0bc07d06b4a69308d6aa19f2d711d7
Gabriel Vieira <gluisvieira@gmail.com>
2016-07-31 19:18:42 -0300

    Implement neighborhood parsing WIP

--- geocoding.rb
+++ geocoding.rb
@@ -1,5 +1,37 @@
 require "pry"
 require "json"
+# require "unicode_utils/upcase"
+require "I18n"
+
+I18n.available_locales = [:en]
+def sanitize(str)
+  I18n.transliterate(str).upcase
+end
+
+def load_neighborhoods(filename)
+  neighborhoods = []
+  text = File.read(filename)
+  text.each_line do |line|
+    neighborhoods << sanitize(line).upcase
+  end
+  neighborhoods
+end
+
+neighborhoods = load_neighborhoods("neighborhoods.txt")
+
+def load_neighborhoods_aliases(filename)
+  aliases = {}
+  text = File.read(filename)
+  text.each_line do |line|
+    str = sanitize(line).upcase
+    child, parents_str = str.split("=>")
+    parents = parents_str.split(/,| E |,E /)
+    aliases[child.strip] = parents.map(&:strip)
+  end
+  aliases
+end
+
+aliases = load_neighborhoods_aliases("neighborhoods_alias.txt")
 
 class Parser
   attr :result, :errors
@@ -8,14 +40,40 @@ class Parser
     @data = JSON.parse(File.read("indicacoes-2013-2016-1469924392.json"))
     @@results = []
     @@errors = []
+    @@neighborhoods = load_neighborhoods("neighborhoods.txt")
+    @@aliases = load_neighborhoods_aliases("neighborhoods_alias.txt")
+  end
+
+  def load_neighborhoods(filename)
+    neighborhoods = []
+    text = File.read(filename)
+    text.each_line do |line|
+      neighborhoods << sanitize(line).upcase
+    end
+    neighborhoods
+  end
+
+  def load_neighborhoods_aliases(filename)
+    aliases = {}
+    text = File.read(filename)
+    text.each_line do |line|
+      str = sanitize(line).upcase
+      child, parents_str = str.split("=>")
+      parents = parents_str.split(/,| E |,E /)
+      aliases[child.strip] = parents.map(&:strip)
+    end
+    aliases
   end
 
   def parse_address(descr)
-    m = /(?<= NA|NO|AO LONGO DA|EXTENSÃO DA)(.*)(?==>)/.match(descr)
-    if m.nil?
-      "ERROR"
-    else
-      m[0]
+    # m = /(?<= NA|NO|AO LONGO DA|EXTENSÃO DA)(.*)(?==>)/.match(descr)
+    # if m.nil?
+    #   "ERROR"
+    # else
+    #   m[0]
+    # end
+    @@neighborhoods.each do |neigh|
+      neigh.
     end
   end
 
@@ -49,6 +107,6 @@ end
 
 parser = Parser.new("indicacoes-2013-2016-1469924392.json")
 parser.parse
-parser.print_results
+# parser.print_results
 
-# pry
+pry
