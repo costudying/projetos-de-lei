3845da75fd9ed09727a121f1d316f14266f7a18f
Gabriel Vieira <gluisvieira@gmail.com>
2016-08-15 12:10:49 -0300

    WIP: map each name in 'neighborhoods' (from 'indications.js') to a name in 'neighborhoodNames.js' (extracted from the KML files)

--- js/mapa.js
+++ js/mapa.js
@@ -1,5 +1,5 @@
 var names = _.keys(indications);
-console.log(names);
+// console.log(names);
 _.each(names, function (name) {
     $("#select-counselor").append($("<option>", {value: name, text: name}));
 });
@@ -43,22 +43,126 @@ var map = new mapboxgl.Map({
     zoom: 13,
     hash: true
 });
-console.log('hi');
-
 map.on('load', function () {
-    $.ajax('bairros-rj-kml/Flamengo.kml').done(function(xml) {
-        var collec = toGeoJSON.kml(xml);
-        var feat = collec.features[0];
-        map.addSource('flamengo', {type: 'geojson', data: feat});
-        map.addLayer({
-            'id': 'neighborhood-flamengo',
-            'type': 'fill',
-            'source': 'flamengo',
-            'layout': {},
-            'paint': {
-                'fill-color': '#F00',
-                'fill-opacity': 0.8
+    console.log("[*] Map loaded.");
+    // var neighborhoodXML = {};
+    // if (localStorage.getItem("neighborhoodXML") === null) {
+    var randomColor = function () {
+        return ('#' + (function co(lor){   return (lor += [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]) && (lor.length == 6) ?  lor : co(lor); })('')).toUpperCase();
+    };
+    var nameQueue = _.clone(neighborhoodNames);
+    var loadNeighborhoods = function (queue, completionCallback) {
+        if (_.isEmpty(queue)) {
+            console.log("[*] Finish loading all neighborhood KML files.");
+            completionCallback();
+            return;
+        } else {
+            var name = queue.pop();
+            console.log("[*] Loading '"+name+".kml'... ("+queue.length+" left)");
+            $.ajax("bairros-rj-kml/" + name + ".kml").done(function (xml) {
+                console.log("[*] Done loading '"+name+".kml!'");
+                // neighborhoodXML[name] = xml;
+                var collec = toGeoJSON.kml(xml);
+                var feat = collec.features[0];
+                map.addSource(name, {type: "geojson", data: feat});
+                map.addLayer({
+                    id: name,
+                    type: "fill",
+                    source: name,
+                    layout: {},
+                    paint: {
+                        "fill-color": randomColor(),
+                        "fill-opacity": 0.8
+                    }
+                });
+                loadNeighborhoods(queue, completionCallback);
+            });
+        }
+    };
+
+    loadNeighborhoods(nameQueue, function () {
+        // localStorage.setItem("neighborhoodXML", JSON.stringify(neighborhoodXML));
+    });
+
+
+    //
+    // WIP: map each name in `neighborhoods` (from `indications.js`) to a name in `neighborhoodNames` (from `neighborhoodNames.js`, extracted from the KML files)
+    //
+    var test = function () {
+        var result = {ok: [], err: []};
+        var nclone = _.clone(neighborhoods);
+        var namesFromInd =
+            _.chain(nclone)
+            .sortBy()
+            .each(function (n) {
+                if (n.search("\\+")!=-1) {
+                    _.each(n.split("+"), function (word) {
+                        nclone.push(word);
+                    });
+                }
+             })
+             .filter(function (n) {
+                 return n.search("\\+")===-1;
+             })
+             .map(function (n) {
+                 return n.split(" ").join("");
+             })
+             .map(function (n) { // correction
+                 if (n === "GARDENIA") { return "GARDENIAAZUL"; }
+                 else if (n === "LAPA") { return "CENTRO"; } // NOT 100% accurate!
+                 else if (n === "ILHADOGOVERNADOR") { return "GALEAO"; } // NOT 100% accurate!
+                 else if (n === "OSWALDOCRUZ") { return "OSVALDOCRUZ"; }
+                 else if (n === "QUINTINO") { return "QUINTINOBOCAIUVA" }
+                 else if (n === "SULACAP") { return "JARDIMSULACAP"; }
+                 else return n;
+             })
+             .value();
+        var namesFromKML = _.chain(neighborhoodNames).sortBy().map(function (n) { return n.toUpperCase(); }).value();
+        var kml = {};
+        _.each(namesFromKML, function (n) {
+            kml[n] = true;
+        });
+        _.each(namesFromInd, function (nind) {
+            if (kml[nind]) {
+                result.ok.push(nind);
+            } else {
+                result.err.push(nind);
             }
         });
-    });
+        return result;
+    };
+    // } else {
+    //     console.log("[*] Loading neighborhood XML files from localStorage.");
+    //     neighborhoodXML = JSON.parse(localStorage.getItem("neighborhoodXML"));
+    //     _.forOwn(neighborhoodXML, function (xml, name) {
+    //         var collec = toGeoJSON.kml(xml);
+    //         var feat = collec.features[0];
+    //         map.addSource(name, {type: "geojson", data: feat});
+    //         map.addLayer({
+    //             id: name,
+    //             type: "fill",
+    //             source: name,
+    //             layout: {},
+    //             paint: {
+    //                 "fill-color": "#F00",
+    //                 "fill-opacity": 0.8
+    //             }
+    //         });
+    //     });
+    // }
+    // $.ajax('bairros-rj-kml/Flamengo.kml').done(function(xml) {
+    //     var collec = toGeoJSON.kml(xml);
+    //     var feat = collec.features[0];
+    //     map.addSource('flamengo', {type: 'geojson', data: feat});
+    //     map.addLayer({
+    //         'id': 'neighborhood-flamengo',
+    //         'type': 'fill',
+    //         'source': 'flamengo',
+    //         'layout': {},
+    //         'paint': {
+    //             'fill-color': '#F00',
+    //             'fill-opacity': 0.8
+    //         }
+    //     });
+    // });
 });
