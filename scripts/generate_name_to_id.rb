17d8797b246bc1506b26a78f7250a6c93cd8214b
Gabriel Vieira <gluisvieira@gmail.com>
2016-09-25 17:37:03 -0300

    Fix generate_name_to_id script

--- scripts/generate_name_to_id.rb
+++ scripts/generate_name_to_id.rb
@@ -9,13 +9,14 @@ def normalize(name)
 end
 
 def generate_name_to_id
-  indications_by_politician = JSON.parse(File.read("../data/indications_by_politician.json"))
+  indications_by_politician = JSON.parse(File.read("fixed_indications_by_politician.json"))
+  laws_by_politician = JSON.parse(File.read("laws_by_politician.json"))
   politicians = JSON.parse(File.read("../dadosVereadores/dados.json"))
 
   # names in `politicians` are in a different format from those in `indications_by_politician`
   # let's say the ones in `politicians` are the "names" and the other ones are the "ids"
   names = politicians.map { |poli| poli["vereador"] }
-  ids = indications_by_politician.keys.select { |id| !id.start_with? "COMISSAO" }
+  ids = indications_by_politician.keys.concat(laws_by_politician.keys).uniq.select { |id| id.start_with? "VEREADOR" }
   names_to_id = {}
 
   fuzzy_match = FuzzyMatch.new(ids)
